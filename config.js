394fc4b85f253f45da6bb46c0c3f6b94522f5c78
klystudio <klybaguio@gmail.com>
2026-02-05 14:27:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://drive.google.com/file/d/1-lbIO0KoupW6ZRn1sg5bbcfIiCIt4p9Y/view?usp=drivesdk", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dv5clzqcx/video/upload/v1770272618/Please_Please_Please_f66kbv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
